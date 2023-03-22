9805df52fcd69c4c9d14fc182f5826d8a7075047
Hussein Barakat <121978208+Husseinb23@users.noreply.github.com>
2023-03-22 02:18:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print("How Smart Are You Machine 🧠:")
+Name = input("Who are you?:")
+Subject = input("Whats your favorite subject?")
+Scale = input("On a scale of 1-10 how good are you at that subject? (1:😢, 10:😎")
+if Scale == "1" or Scale == "2" or Scale == "3":
+  print("Oof",Name,"Don't worry, a 1-3 score just means there's plenty of room for improvement!")
+elif Scale == "4" or Scale == "5" or Scale == "6":
+ print("Its alright",Name,"everyone has to start somewhere, and you're making progress! Keep up the hard work and stay committed to improving your skills!")
+elif Scale == "8" or Scale == "9" or Scale == "10":
+ print("Dang!!!",Name,"A 8-10 score? Impressive! It's clear that you have a deep understanding of the subject")
+  
+
+
+
